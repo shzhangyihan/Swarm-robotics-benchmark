8883ec70cbebdae318c272493db70dd1f91cd0ee
Yihan Zhang <shzhangyihan@gmail.com>
2019-08-27 00:25:36 -0500

    update template

--- Template/Kilobot_simulation/kilobot.cpp
+++ Template/Kilobot_simulation/kilobot.cpp
@@ -1,15 +1,7 @@
 #pragma once
 #include "../../../Swarm-robotics-benchmark/Driver/Kilobot_simulation/kilobot_driver.h"
 
-class mykilobot : public kilobot_driver
-{
-
-/*----------------------------------------------------------------------*/
-/*-----------------------------           ------------------------------*/
-/*----------------------------- User code ------------------------------*/
-/*-----------------------------           ------------------------------*/
-/*----------------------------------------------------------------------*/
-
+class mykilobot : public kilobot_driver {
     Channel *channel;
     Publisher *publisher;
     Subscriber *subscriber; 
@@ -18,26 +10,21 @@ class mykilobot : public kilobot_driver
     void sent() {
         publisher->send(msg1, strlen((char*)msg1));
     }
-
     
     void call_back(unsigned char * msg, int size, int hop, Meta_t * meta) {
-    	printf("M Recv dist = %d, theta = %f\n", meta->dist, meta->theta);
+        printf("M Recv dist = %d, theta = %f\n", meta->dist, meta->theta);
     } 
     
-
-	void loop()
-	{
-	}
-
-	//executed once at start
-	void setup()
-	{
+    void loop() {
+        
+    }
+    
+    void setup() {
         strcpy((char*)msg1, "H");
-		channel = swarmnet.new_channel(2, 0, false);
-		publisher = channel->new_publisher(sent);
+        channel = swarmnet.new_channel(2, 0, false);
+        publisher = channel->new_publisher(sent);
         subscriber = channel->new_subscriber(100, call_back);
-
-		publisher->send(msg1, strlen((char*)msg1));
+        publisher->send(msg1, strlen((char*)msg1));
         set_color(RGB(0,0,1));
-	}
+    }
 };
