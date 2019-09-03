d93dbd1e27126313ed469e109edc7629e7d0c20e
shzhangyihan <shzhangyihan@gmail.com>
2019-09-03 15:21:45 -0500

    first kilo test passed

--- Benchmark/simple_test/user_program.h
+++ Benchmark/simple_test/user_program.h
@@ -4,24 +4,23 @@ Channel *channel;
 Publisher *publisher;
 Subscriber *subscriber; 
 unsigned char token[20];
+int rand_id;
 
 void sent() {
-    //printf("Sent\n");
     publisher->send(token, strlen((char*)token));
-    //set_color(RGB(1, 0, 0));
 }
 
 void call_back(unsigned char * msg, int size, int hop, Meta_t * meta) {
-    printf("Recv dist = %d, size = %d, payload = %s\n", meta->dist, size, msg);
-    //set_color(RGB(0, 1, 0));
+    printf("Recv dist = %d, size = %d, payload = %s, from = %d\n", meta->dist, size, (msg + 1), (int)msg[0]);
 } 
 
 void loop() {
-    //printf("loop\n");
 }
 
 void setup() {
-    strncpy((char*)token, "Hello world!", 12);
+    rand_id = rand_hard();
+    strncpy((char*)token, " Hello world!", 13);
+    token[0] = rand_id;
     channel = swarmnet->new_channel(2, 0, false);
     publisher = channel->new_publisher(sent);
     subscriber = channel->new_subscriber(100, call_back);
