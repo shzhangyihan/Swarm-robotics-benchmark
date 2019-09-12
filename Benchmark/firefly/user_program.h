19c44b8dbd423e9f330cc547c86bbff4851cd2c7
Yihan Zhang <shzhangyihan@gmail.com>
2019-09-12 03:37:07 -0500

    firefly first try

--- Benchmark/firefly/user_program.h
+++ Benchmark/firefly/user_program.h
@@ -1,45 +1,76 @@
 #pragma once
-#include "../../../Swarm-robotics-benchmark/Driver/Kilobot_simulation/kilobot_driver.h"
+#include <math.h>
 
 START_USER_PROGRAM
 
-Channel *channel;
-Publisher *publisher;
-Subscriber *subscriber; 
-unsigned char msg1[10];
+#define WHOLE_PERIOD 1000
+#define FLASH_PERIOD 50
+#define NORMALIZE_SCALE WHOLE_PERIOD / 50
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
-void sent() {
-    publisher->send(msg1, strlen((char*)msg1));
+typedef struct state {
+    int period;
+} state_t;
+
+Channel * channel;
+Publisher * publisher;
+Subscriber * subscriber;
+state_t my_state;
+int current_period_start;
+int total_period_diff;
+int total_period_count;
+
+void sent_callback() {
+    my_state.period = swarmos.get_clock() - current_period_start;
+    publisher->send((unsigned char *) &my_state, sizeof(my_state));
 }
 
-void call_back(unsigned char * msg, int size, int hop, Meta_t * meta) {
-    if (msg[0] == 'A'){
-        timer += (timer/100)*(timer/100);
+int normalize_period_diff(int avg_period_diff) {
+    return NORMALIZE_SCALE * (avg_period_diff + WHOLE_PERIOD) / (2 * WHOLE_PERIOD);
+}
+
+void recv_callback(unsigned char * msg, int size, int ttl, Meta_t * meta) {
+    state_t * recv_state = (state_t *) msg;
+    int current_source_period = (recv_state->period + meta->msg_delay) % WHOLE_PERIOD;
+    if(current_source_period < 0) {
+        // source clock overflow
+        return;
     }
-} 
+    int period_diff = (swarmos.get_clock() - current_period_start) - current_source_period;
+    // to consider period rollover
+    period_diff = MIN(abs(period_diff), MIN(abs(period_diff + WHOLE_PERIOD), abs(period_diff - WHOLE_PERIOD)));
+    
+    if(period_diff > 500)
+    total_period_diff = total_period_diff + period_diff;
+    total_period_count = total_period_count + 1;
+}
 
 void loop() {
-    if (timer >= 500){
-        timer = 0;
-        strcpy((char*)msg1, "A");
+    if(swarmos.get_clock() - current_period_start > WHOLE_PERIOD) {
+        int period_adjustment;
+        if(total_period_count == 0) period_adjustment = 0;
+        else period_adjustment = normalize_period_diff(total_period_diff / total_period_count);
+        LED_control->turn_on(1, 0, 0, 100);
+        current_period_start = swarmos.get_clock();
+        current_period_start = current_period_start + period_adjustment;
+        total_period_diff = 0;
+        total_period_count = 0;
     }
-    else if (timer < 25) {
-        set_color(RGB(1,1,1));
-        strcpy((char*)msg1, "H");
-    }
-    else {
-        set_color(RGB(0,0,1));
-        strcpy((char*)msg1, "H");
+    if(swarmos.get_clock() - current_period_start > FLASH_PERIOD) {
+        LED_control->turn_on(0, 0, 1, 100);
     }
 }
 
 void setup() {
-    strcpy((char*)msg1, "H");
-    channel = swarmnet.new_channel(2, 0, false);
-    publisher = channel->new_publisher(sent);
-    subscriber = channel->new_subscriber(100, call_back);
-    publisher->send(msg1, strlen((char*)msg1));
-    set_color(RGB(0,0,1));
+    channel = swarmnet->new_channel(0, 0, true);
+    publisher = channel->new_publisher(sent_callback);
+    subscriber = channel->new_subscriber(200, recv_callback);
+
+    current_period_start = swarmos.get_clock();
+    my_state.period = swarmos.get_clock() - current_period_start;
+
+    publisher->send((unsigned char *) &my_state, sizeof(my_state));
+    LED_control->turn_on(0, 0, 1, 100);
 }
 
 END_USER_PROGRAM
