c02fb445306bb81419b5ab8ae25f039278829207
Yihan Zhang <shzhangyihan@gmail.com>
2019-10-24 04:48:19 -0500

    firefly rewrite

--- Benchmark/firefly/user_program.h
+++ Benchmark/firefly/user_program.h
@@ -3,65 +3,64 @@
 
 START_USER_PROGRAM
 
-#define WHOLE_PERIOD 1000
-#define FLASH_PERIOD 5
-#define NORMALIZE_SCALE 0.1
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define PERIOD 50
+#define LED_DURATION 2
+#define TIMER_CHANGE_BOUND 20
+#define CHANGE_NORMAL 25
 
 typedef struct state {
-    int period;
+    int cur_period;
 } state_t;
 
 Channel * channel;
 Publisher * publisher;
 Subscriber * subscriber;
 state_t my_state;
-int current_period_start;
-int current_period_adjustment;
-int total_period_diff;
-int total_period_count;
+int last_reset;
+int timer_change;
+int timer_change_raw;
 
 void sent_callback() {
-    my_state.period = swarmos.get_clock() - current_period_start + current_period_adjustment;
+    my_state.cur_period = swarmos.get_clock() - last_reset;
     publisher->send((unsigned char *) &my_state, sizeof(my_state));
-}
-
-int normalize_period_diff(int avg_period_diff) {
-    if(avg_period_diff > 0) {
-        return NORMALIZE_SCALE * (WHOLE_PERIOD - avg_period_diff);
-    }
-    else {
-        return NORMALIZE_SCALE * ((-1) * avg_period_diff);
-    }
 }
 
 void recv_callback(unsigned char * msg, int size, int ttl, Meta_t * meta) {
     state_t * recv_state = (state_t *) msg;
-    int current_source_period = (recv_state->period + meta->msg_delay) % WHOLE_PERIOD;
-    if(current_source_period < 0) {
-        // source clock overflow
-        return;
+    int recv_period = (recv_state->cur_period + meta->msg_delay) % PERIOD;
+    int my_period = swarmos.get_clock() - last_reset;
+    // printf("period_diff = %d delay = %d\n", my_period - recv_period, meta->msg_delay);
+    printf("recv %02d - recv_period = %d delay = %d my_period = %d\n", id, recv_period, meta->msg_delay, my_period);
+    int tmp_timer_change_raw = timer_change_raw;
+    if((my_period - recv_period) > 0) {
+        if((my_period - recv_period) < (PERIOD / 2)) {
+            timer_change_raw += my_period - recv_period;
+        }
+        else {
+            timer_change_raw -= (PERIOD + recv_period - my_period) % PERIOD;
+        }
     }
-    int period_diff = (swarmos.get_clock() - current_period_start + current_period_adjustment) - current_source_period;
-    // to consider period rollover
-    period_diff = MIN(abs(period_diff), MIN(abs(period_diff + WHOLE_PERIOD), abs(period_diff - WHOLE_PERIOD)));
-    //printf("%d: period_diff: %d\n", id, period_diff);
-    total_period_diff = total_period_diff + period_diff;
-    total_period_count = total_period_count + 1;
+    else if((my_period - recv_period) < 0) {
+        if((recv_period - my_period) < (PERIOD / 2)) {
+            timer_change_raw -= recv_period - my_period;
+        }
+        else {
+            timer_change_raw += (PERIOD + my_period - recv_period) % PERIOD;
+        }
+    }
+    printf("recv %02d - timer_change_raw from %d to %d\n", id, tmp_timer_change_raw, timer_change_raw);
 }
 
 void loop() {
-    //printf("loop\n");
-    my_state.period = swarmos.get_clock() - current_period_start + current_period_adjustment;
-    if(my_state.period > WHOLE_PERIOD) {
-        if(total_period_count == 0) current_period_adjustment = normalize_period_diff(0);
-        else current_period_adjustment = normalize_period_diff(total_period_diff / total_period_count);
-        LED_control->turn_on(1, 1, 1, FLASH_PERIOD);
-        current_period_start = swarmos.get_clock();
-        my_state.period = current_period_adjustment;
-        printf("%d: flash at %d, avg error %d adjustment %d\n", id, current_period_start, (total_period_count == 0) ? 0: (total_period_diff / total_period_count), current_period_adjustment);
-        total_period_diff = 0;
-        total_period_count = 0;
+    int reset_time = last_reset + PERIOD + timer_change;
+    printf("%02d - kilo_ticks = %d last_reset = %d timer_change_raw = %d timer_change = %d reset_time = %d \n", id, swarmos.get_clock(), last_reset, timer_change_raw, timer_change, reset_time);
+    if(swarmos.get_clock() >= reset_time) {
+        last_reset = swarmos.get_clock();
+        timer_change = timer_change_raw / CHANGE_NORMAL;
+        if(timer_change > TIMER_CHANGE_BOUND) timer_change = TIMER_CHANGE_BOUND;
+        if(timer_change < -TIMER_CHANGE_BOUND) timer_change = -TIMER_CHANGE_BOUND;
+        LED_control->turn_on(1, 1, 1, LED_DURATION);
+        timer_change_raw = 0;
     }
 }
 
@@ -70,9 +69,9 @@ void setup() {
     publisher = channel->new_publisher(sent_callback);
     subscriber = channel->new_subscriber(200, recv_callback);
 
-    current_period_start = swarmos.get_clock();
-    current_period_adjustment = swarmos.random_func() % WHOLE_PERIOD;
-    my_state.period = swarmos.get_clock() - current_period_start + current_period_adjustment;
+    last_reset = 0;
+    timer_change = 0;
+    my_state.cur_period = swarmos.get_clock() - last_reset;
 
     publisher->send((unsigned char *) &my_state, sizeof(my_state));
     LED_control->turn_on(0, 0, 0, 0);
